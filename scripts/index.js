d379c1f1370ff2aaf5542c271622ac38c14a7443
Artjom Gussev <artjomgussev@Artjoms-MacBook-Pro.local>
2022-06-30 16:23:38 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,22 @@
+const exp = require("constants");
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
-function pConsole() {
-  let data = document.getElementById("name");
-  console.log("Your name:" + data.value);
-}
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../views"));
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+app.get("/form.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/form.html"));
+});
+app.get("/result.html", function (req, res) {
+  res.sendFile(path.join(__dirname, "../views/result.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
